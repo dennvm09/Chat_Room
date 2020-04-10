2be53c181dab2a95217a65a1ca96f42d19884143
dennvm09 <valenmosquera1999@gmail.com>
2020-04-10 05:30:22 -0500

    learning how to implement sockets using flask

---
+++ main.py
@@ -0,0 +1,27 @@
+#render_template plantillas, helps in rendering the HTML page which are so called the templates
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
+
+@app.route('/') #la ruta a la que va a responder
+def sessions():
+    return render_template('session.html') #el nombre de mi html
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
+
+#socketio variable which enables us to use socketio instead of app in running the application
+socketio = SocketIO(app)
+#The run() method takes optional host and port arguments
+#by default it will listen on localhost:5000 like Flask’s development web server. 
+#debug=True enables to sort out the errors with ease.
+if __name__ == '__main__':
+    socketio.run(app, debug=True)
